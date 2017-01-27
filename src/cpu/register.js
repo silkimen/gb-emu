05b8b7ee54f19b5cd1227a5e3f1990a566567085
Sefa Ilkimen <sefa@ilkimen.net>
2017-01-27 23:59:01 +0100

    Using array buffer for registers

--- src/cpu/register.js
+++ src/cpu/register.js
@@ -2,32 +2,30 @@ const REG_NAMES = [ 'a', 'b', 'c', 'd', 'e', 'f', 'h', 'l', 'sp', 'pc' ];
 const FLAG_NAMES = [ 'zero', 'subtract', 'half', 'carry' ];
 const FLAG_MASKS = [ 0x80, 0x40, 0x20, 0x10 ];
 
-const register = {};
-const flag = {};
+export const register = {};
+export const flag = {};
+export const reset = () => Object.keys(register).forEach(key => register[key] = 0);
 
-const reset = () => Object.keys(register).forEach(key => register[key] = 0);
+const registerBuffer = new ArrayBuffer(REG_NAMES.length);
+const registerView = new Uint8Array(registerBuffer);
 
 const getFlag = mask => (register.f & mask) > 0;
-
 const setFlag = (value, mask) => {
   register.f = value === true ? register.f | mask : register.f & ~mask;
 };
 
-REG_NAMES.forEach(name => {
+REG_NAMES.forEach((name, index) => {
   Object.defineProperty(register, name, {
     enumerable: true,
-    writable: true,
-    value: 0
+    get: () => registerView[index],
+    set: value => registerView[index] = value
   });
 });
 
 FLAG_NAMES.forEach((name, index) => {
   Object.defineProperty(flag, name, {
     enumerable: true,
-    writable: true,
     get: () => getFlag(FLAG_MASKS[index]),
     set: value => setFlag(value, FLAG_MASKS[index])
   });
 });
-
-export { register, flag, reset };
