fee34cd43282c2f93579e6d33bd1d1a75983115b
Sefa Ilkimen <sefa@ilkimen.net>
2017-03-19 03:06:07 +0100

    Fixed register implementation for SP and PC

--- spec/cpu/register.partial.js
+++ spec/cpu/register.partial.js
@@ -1,20 +1,23 @@
 const Register = require(`${__base}cpu/register`).Register;
 
-const REG_NAMES = [ 'a', 'b', 'c', 'd', 'e', 'f', 'h', 'l', 'sp', 'pc' ];
+const BYTE_REG_NAMES = [ 'a', 'b', 'c', 'd', 'e', 'f', 'h', 'l' ];
+const WORD_REG_NAMES = [ 'sp', 'pc' ];
 
 let reg = null;
 
-describe('registers', () => {
+describe('register', () => {
   beforeEach(() => {
     reg = new Register();
   });
 
-  it('are initialized with 0', () => {
-    REG_NAMES.forEach(name => expect(reg.register[name]).toBe(0));
+  BYTE_REG_NAMES.forEach(name => {
+    it(`${name.toUpperCase()} is initialized with 0`, () => {
+      expect(reg.register[name]).toBe(0);
+    });
   });
 
-  it('are representing unsigned 8 bit data', () => {
-    REG_NAMES.forEach(name => {
+  BYTE_REG_NAMES.forEach(name => {
+    it(`${name.toUpperCase()} is representing unsigned 8 bit data`, () => {
       // max value you can represent with 8 bit
       reg.register[name] = 255;
       expect(reg.register[name]).toBe(255);
@@ -28,56 +31,78 @@ describe('registers', () => {
       expect(reg.register[name]).toBe(1);
     });
   });
+
+  WORD_REG_NAMES.forEach(name => {
+    it(`${name.toUpperCase()} is initialized with 0`, () => {
+      expect(reg.register[name]).toBe(0);
+    });
+  });
+
+  WORD_REG_NAMES.forEach(name => {
+    it(`${name.toUpperCase()} is representing unsigned 16 bit data`, () => {
+      // max value you can represent with 16 bit
+      reg.register[name] = 65535;
+      expect(reg.register[name]).toBe(65535);
+
+      // overflow (+1)
+      reg.register[name] = 65536;
+      expect(reg.register[name]).toBe(0);
+
+      // overflow (+2)
+      reg.register[name] = 65537;
+      expect(reg.register[name]).toBe(1);
+    });
+  });
 });
 
-describe('flags', () => {
+describe('flag', () => {
   beforeEach(() => {
     reg = new Register();
   });
 
-  it('zero flag reads correctly from register F', () => {
+  it('"zero" reads correctly from register F', () => {
     expect(reg.flag.zero).toBe(false);
     reg.register.f = 0b10000000;
     expect(reg.flag.zero).toBe(true);
   });
 
-  it('subtract flag reads correctly from register F', () => {
+  it('"subtract" reads correctly from register F', () => {
     expect(reg.flag.subtract).toBe(false);
     reg.register.f = 0b01000000;
     expect(reg.flag.subtract).toBe(true);
   });
 
-  it('half-carry flag reads correctly from register F', () => {
+  it('"half-carry" reads correctly from register F', () => {
     expect(reg.flag.half).toBe(false);
     reg.register.f = 0b00100000;
     expect(reg.flag.half).toBe(true);
   });
 
-  it('carry flag reads correctly from register F', () => {
+  it('"carry" reads correctly from register F', () => {
     expect(reg.flag.carry).toBe(false);
     reg.register.f = 0b00010000;
     expect(reg.flag.carry).toBe(true);
   });
 
-  it('zero flag updates register F correctly', () => {
+  it('"zero" updates register F correctly', () => {
     expect(reg.flag.zero).toBe(false);
     reg.flag.zero = true;
     expect(reg.register.f).toBe(0b10000000);
   });
 
-  it('subtract flag updates register F correctly', () => {
+  it('"subtract" updates register F correctly', () => {
     expect(reg.flag.subtract).toBe(false);
     reg.flag.subtract = true;
     expect(reg.register.f).toBe(0b01000000);
   });
 
-  it('half-carry flag updates register F correctly', () => {
+  it('"half-carry" updates register F correctly', () => {
     expect(reg.flag.half).toBe(false);
     reg.flag.half = true;
     expect(reg.register.f).toBe(0b00100000);
   });
 
-  it('carry flag flag updates register F correctly', () => {
+  it('"carry" updates register F correctly', () => {
     expect(reg.flag.carry).toBe(false);
     reg.flag.carry = true;
     expect(reg.register.f).toBe(0b00010000);
--- src/cpu/register.js
+++ src/cpu/register.js
@@ -1,4 +1,5 @@
-const REG_NAMES = [ 'a', 'b', 'c', 'd', 'e', 'f', 'h', 'l', 'sp', 'pc' ];
+const BYTE_REGISTER_NAMES = [ 'a', 'b', 'c', 'd', 'e', 'f', 'h', 'l' ];
+const WORD_REGISTER_NAMES = [ 'sp', 'pc' ];
 const FLAG_NAMES = [ 'zero', 'subtract', 'half', 'carry' ];
 const FLAG_MASKS = [ 0x80, 0x40, 0x20, 0x10 ];
 
@@ -6,20 +7,33 @@ export const Register = function () {
   const register = {};
   const flag = {};
 
-  const registerBuffer = new ArrayBuffer(REG_NAMES.length);
-  const registerView = new Uint8Array(registerBuffer);
+  const byteBuffer = new ArrayBuffer(BYTE_REGISTER_NAMES.length);
+  const byteView = new Uint8Array(byteBuffer);
+
+  const wordBuffer = new ArrayBuffer(WORD_REGISTER_NAMES.length * 2);
+  const wordView = new Uint16Array(wordBuffer);
 
   const getFlag = mask => (register.f & mask) > 0;
   const setFlag = (value, mask) => {
     register.f = value === true ? register.f | mask : register.f & ~mask;
   };
 
-  REG_NAMES.forEach((name, index) => {
+  BYTE_REGISTER_NAMES.forEach((name, index) => {
     Object.defineProperty(register, name, {
       enumerable: true,
-      get: () => registerView[index],
+      get: () => byteView[index],
       set: value => {
-        registerView[index] = value;
+        byteView[index] = value;
+      }
+    });
+  });
+
+  WORD_REGISTER_NAMES.forEach((name, index) => {
+    Object.defineProperty(register, name, {
+      enumerable: true,
+      get: () => wordView[index],
+      set: value => {
+        wordView[index] = value;
       }
     });
   });
